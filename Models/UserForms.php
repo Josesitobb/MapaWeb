bd1a2dc67906d8fc62ce8199d1f0b7e729827009
Josesitobb <joseguerra.jg683@gmail.com>
2025-05-25 13:18:15 -0500

    Aregar get y set y crear consulta

--- Models/UserForms.php
+++ Models/UserForms.php
@@ -4,40 +4,69 @@ require_once('../Models/Database.php');
 class User
 {
     private $db;
-    private $Username;
-    private $UserDescription;
-    private $UserData;
-    private $UserSpot;
+    //  1
+    private $Program;
+    // 2
+    private $Semester;
+    // 3
+    private $Day;
+    // 4
+    private $Age;
+    // 5
+    private $Locality;
+    // 6
+    private $Neighborhood;
+    // 7
+    private $Transport;
+    // 8
+    private $Perception;
+    // 9
+    // AQUI VA LA DE LAS 3 PREGUNTAS DEL la pregunta "
+    //     Sobre la percepción de seguridad en zonas aledañas a la universidad, de las siguientes asigne un valor entre 0 y 5, siendo 5 muy segura  y 0 
+    // POCO segura
+    private $AvRojas26;
+    private $AvRojas63;
+    private $AvRojas53yBoyaca;
+    // 10
+    private $Intimidation;
+    // 11
+    private $MapTheft = [];
+    // 12
+    private $DayFacts;
+    // 13
+    private $TimeFacts;
+    // 14
+    private $VictimOfTheft;
+    // 15
+    // ¿En el hecho intervinieron cuantos delincuentes?
+    private $NumberOfOffenders;
+    // 16
+    private $WeaponType;
+    // 17
+    private $DisplacementOfPerpetrators;
+    // 18
+    private $HowWaShe;
+    // 19
+    private $typeOfLesson;
+    // 20
+    private $AfterTheEvents;
+    // 21
+    private $Recommendation;
+    // 22
 
-    public function __construct($db, $Username, $UserDescription, $UserData, $UserSpot)
+
+
+
+
+    public function __construct($db)
     {
         $this->db =  $db->Conection();
-        $this->Username = $Username;
-        $this->UserDescription = $UserDescription;
-        $this->UserData = $UserSpot;
     }
 
     // Metodos get y set
 
     // Get
-    public function getUsername()
-    {
-        return $this->Username;
-    }
 
-    public function getUserDescription()
-    {
-        return $this->UserDescription;
-    }
-
-    public function getUserData()
-    {
-        return $this->UserData;
-    }
-
-    public function getUserSpot(){
-        return $this->getUserSpot();
-    }
 
 
     // Crear Encuentas
@@ -46,10 +75,7 @@ class User
         try {
             $sql = 'INSER INTO  dataformsuser(UserName,UserDescription,UserDate,UserSpot)VALUE( :UserName, :UserDescription ,:UserDate,:UserSpot ) ';
             $stmtUser = $this->db->prepare($sql);
-            $stmtUser -> bindValue(':UserName', $this->getUsername());
-            $stmtUser -> bindValue(':UserDescription', $this->getUserDescription());
-            $stmtUser -> bindValue(':UserDate', $this->getUserData());
-            $stmtUser -> bindValue(':UserSpot', $this->getUserSpot());
+
             $stmtUser->execute();
         } catch (Exception $e) {
             die($e->getMessage());
